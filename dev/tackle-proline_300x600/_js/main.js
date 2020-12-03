9a2d2c89f6a5fd4986b6efe62f0ce1740f155c16
Gar <gar@lonelydatu.com>
2020-12-03 15:26:06 -0500

    on

---
+++ dev/tackle-proline_300x600/_js/main.js
@@ -0,0 +1,31 @@
+import {size, frameEnd, init} from '../../_common/js/common.js'
+import {player} from '../../_common/js/striker.js'
+
+
+function start(){
+	
+	const tl = init()
+	// return
+	const {w, h} = size
+	tl.set(".ill", {transformOrigin:`${w/2}px ${h/2}px`, x:0, y:0 })
+
+	tl.from(".player2", 1, {x:"-=70", y:"-=40"}, 0)
+	player(1)
+	tl.from(".t1a", .1, {x:"-=300",opacity:1}, 1.0)
+	tl.from(".t1b", .01, {opacity:0}, "+=1")
+
+	
+	
+	
+	const phone = new TimelineMax()
+	phone.from(".phone", .3, {y:"+=100", opacity:0}, "+=.3")
+	tl.add(frameEnd({phone}), "+=2")
+
+}
+
+
+start()
+
+
+module.exports = {};
+
