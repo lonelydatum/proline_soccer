0783dd51dbce51551e6c8a3ac0cc055144b4f10d
Gar <gar@lonelydatu.com>
2020-12-03 22:20:12 -0500

    on

---
+++ dev/tackle-pool_728x90/_js/main.js
@@ -0,0 +1,34 @@
+import {size, frameEndPool, init} from '../../_common/js/common.js'
+import {player} from '../../_common/js/striker.js'
+
+
+function start(){
+	
+	const tl = init("728x90")
+	// return
+	const {w, h} = size
+
+	tl.set(".ill", {transformOrigin:`${w*.25}px ${h}px`, x:0, y:0 })
+	
+	
+
+	tl.to(".player2", 1, {x:0, y:0}, 0)
+	player(1)
+	tl.from(".t1a", .15, {x:"-=400",opacity:0}, 1.0)
+	tl.from(".t1b", .01, {opacity:0}, "+=1")
+
+	
+	
+	
+	
+	
+	tl.add(frameEndPool(), "+=2")
+
+}
+
+
+start()
+
+
+module.exports = {};
+
---
+++ dev/tackle-pool_970x250/_js/main.js
@@ -0,0 +1,30 @@
+import {size, frameEndPool, init} from '../../_common/js/common.js'
+import {player} from '../../_common/js/striker.js'
+
+
+function start(){
+	
+	const tl = init("970x250")
+	// return
+	const {w, h} = size
+	tl.set(".ill", {transformOrigin:`${w*.25}px ${h}px`, x:0, y:0 })
+	
+
+	tl.to(".player2", 1, {x:0, y:0}, 0)
+	player(1.6)
+	tl.from(".t1a", .2, {x:"-=350",opacity:0}, 1.0)
+	tl.from(".t1b", .01, {opacity:0}, "+=1")
+
+	
+	
+	
+	tl.add(frameEndPool(), "+=2")
+
+}
+
+
+start()
+
+
+module.exports = {};
+
